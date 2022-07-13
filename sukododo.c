1e51193420fb6152134b3ca75b0d2a3076f427fe
HASHIMOTO Nozomi <s1290142@sshsv212.u-aizu.ac.jp>
2022-07-14 01:57:54 +0900

    HoT:initial revision

---
+++ sukododo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+
+    int count = 0;
+    int num;
+    int i = 0;
+    printf("Tossing a coin...\n");
+    srand(time(NULL));
+    for(i = 0 ; i < 3 ; i++){
+        printf("Round %d: ",i+1);
+        if((rand()%2)==0){
+           printf("Heads\n"); 
+           count++;
+        }else{ 
+    printf("Tails\n");
+    num++;
+    }
+    }
+    printf("Heads: %d, Tails: %d\n",count,num);
+    
+}
